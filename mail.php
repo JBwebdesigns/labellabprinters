a32d281b657d9679291f282a09a8f65384f04775
JBwebdesigns <jonathankbanda0317@gmail.com>
2024-07-28 11:49:27 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -38,7 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     } else {
         $_SESSION['msg'] = '<div class="alert alert-danger" role="alert">Please add a valid email address, name, subject, and message.</div>';
-        header("Location: index.php");
+        header("Location: index.html");
         exit;
     }
 }
